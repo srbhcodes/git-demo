0b38ca659a37b31d2f0bfb3fb50d2fff6298b852
Ronak Parashar <ronakparashar0108@gmail.com>
2025-09-26 00:40:44 +0530

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <div>Acme Corp</div>
+      <div>My Company Name
+      </div>
       <nav>
         <a href="/">Home</a> | <a href="/about">About</a> | <a href="/services">Services</a> | <a href="/contact">Contact</a>
       </nav>
