0df3747f4e0737e2d30f164448c56f852bfd0ebf
Ronak Parashar <ronakparashar0108@gmail.com>
2025-09-25 23:53:45 +0530

    feature-b: rename company to Globex Inc and add Contact link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
-<div>My Company Name</div>import React from 'react';
+<div>Globex Inc</div>import React from 'react';
 
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <div>My Company Name</div>
+      <div>Globex Inc</div>
     <nav>
-  <a href="/">Home</a> | <a href="/about">About</a>
+  <a href="/">Home</a> | <a href="/about">About</a> | <a href="/contact">Contact</a>
 </nav>
     </header>
   );
